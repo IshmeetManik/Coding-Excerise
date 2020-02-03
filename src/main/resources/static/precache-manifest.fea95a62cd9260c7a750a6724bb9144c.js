2e1e9646e6f1b255d4e563f72b4d676ff6899fd8
vb <ishthemanik@gmail.com>
2020-02-03 17:00:56 +0530

    First Commit

---
+++ src/main/resources/static/precache-manifest.fea95a62cd9260c7a750a6724bb9144c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7277fa6aa81371c02be91d72141d7513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94492bbfff74a4e49845",
+    "url": "/static/css/2.c070be82.chunk.css"
+  },
+  {
+    "revision": "4cd5eb2192a82679d967",
+    "url": "/static/css/main.4c4e5265.chunk.css"
+  },
+  {
+    "revision": "94492bbfff74a4e49845",
+    "url": "/static/js/2.2e08dfbe.chunk.js"
+  },
+  {
+    "revision": "4cd5eb2192a82679d967",
+    "url": "/static/js/main.972ca646.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9ff83c5b0e25a0e4dda3a94184d5c42",
+    "url": "/static/media/logo.e9ff83c5.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "6e923c3e68ce3f68511a2c7e54b072d6",
+    "url": "/static/media/sidebar-2.6e923c3e.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
